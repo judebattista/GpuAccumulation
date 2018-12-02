db1939f1cd5de8d45ab4669b48aac01905ddf67f
judebattista <jbattista20@my.whitworth.edu>
2018-12-02 14:43:17 -0800

    Integrate our join solution with the naive solution. Add timers and compare the two.

--- GpuReduction.cu
+++ GpuReduction.cu
@@ -1,11 +1,17 @@
+/*  Authors: Cameron Rutherford and Jude Battista
+*   
+*
+*/
+
 #include <iomanip>
 #include <iostream>
-
+#include <random>
 
 //CUDA Kernel
 //Maps one thread to each output space
 //Reduces the array once by a factor of reductionFactor
 //Assumption: we have enough threads to span the output array for our given reductionFactor
+//This will be hideously inefficient as r approaches n
 __global__ void reduceArraySingleStep(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactor) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     //If we are in our problem space
@@ -28,83 +34,66 @@ __global__ void reduceArraySingleStep(double* fullArray, double* reducedArray, i
 //Optimize memory coherence
 
 //CUDA Kernel
-//Maps one thread to each output space
-//Can reduce the array to a single value
-//Note that this reductionFactor applies to the complete reduction, not a single step.
-//Each thread works over a single section  of the array, so the memory access is poorly optimized
-//Each thread reduces it's section of the array to a single value via multiple iterations, which is then stored in the output array/
-//Much like the single step variant, but solves the problem of insufficient threads to span the array
-__global__ void reduceArrayOutputMap(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactori) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    //if we are in our problem space
-    if (tid < fullArraySize) {
-        //Figure out how many threads are working on the array
-        int numThreads = gridDim.x * blockDim.x;
-        //If numThreads >= reductionFactor, this is just the single step version
-        //Note that this check will be identical throughout a block, preventing divergence
-        if (numThreads >= reductionFactor) {
-            int startingIndex = reductionFactor * tid;
-            double balsamic = 0;
-            for (int ndx = 0; ndx < reductionFactor; ndx++) {
-                balsamic += *(fullArray + ((startingIndex + ndx) % fullArraySize));
-            }
-            balsamic /= reductionFactor;
-            *(reducedArray + tid) = balsamic;
-        } else { //Otherwise each thread will need to perform multiple reductions
-            //Based on the number of threads, figure out the size of the section each thread is responsible for
-            int sectionLen = (fullArraySize + numThreads - 1) / numThreads;
-            //Figure out where this thread should start its section
-            int sectionStart = sectionLen * tid; 
-            int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
-            double gravy = 0;
-            for (int ndx = 0; ndx < reductionFactor; ndx++) {
-                gravy += *(fullArray + ((startingIndex + ndx) % fullArraySize));
-            }
-            gravy /= reductionFactor;
-            *(reducedArray + tid) = gravy;
+//Single block reduction algorithm
+//In order to use multiple blocks, the host can simply split the input array between blocks, call this kernel for each block, then recursively combine the results using this kernel 
+__global__ void reduceBlock(double *idata, double *odata, unsigned int fullArraySize, int data_per_thread, int reducedArraySize) {
+
+    //Shared data array. Holds intermediate reduction values
+    extern __shared__ double sdata[];
+    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int aggregate = 0;
+
+    //Make a first pass, reducing the array to the number of threads
+    for (int i = 0; i < data_per_thread; i++) {
+        //Keep memory coalesced
+        aggregate += idata[tid + (i * (blockDim.x)) % fullArraySize];
+    }
+    //store the results in shared memory, one output per thread
+    sdata[tid] = double(aggregate) / data_per_thread;
+
+    __syncthreads();
+
+    //Now we need to reduce the values in shared memory
+    //Start by reducing the values to the lowest power of two that is greater than our target size
+    int i = 512; //length of shared data at the end of for loop
+    for (; i > reducedArraySize - 1; i >>= 1) {
+        if (tid < i) {
+            sdata[tid] += sdata[tid + i];
+            sdata[tid] /= 2;
         }
     }
-}
 
-
-//CUDA Kernel
-//Can reduce the array to a single value
-//Note that this reductionFactor applies to the complete reduction, not a single step.
-//Each SET of threads spans a single segment, meaning that memory access for a given segment should be contiguous
-//This method does not require having enough threads to span the entire array
-__global__ void reduceArrayOutputMap(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactori) {
-    int numThreads = gridDim.x * blockDim.x;
-    //We want each block of threads to address a specific section of the array of size N / numberOfBlocks
-    //Within that section, we want the set of threads in the block to span a segment of size section / numberOfThreadsPerBlock
-    //  and then move to another segment together until the section is complete
-    //Once each thread as averaged its values, write those values to shared memory
-    //Then thread 0 averages the values in shared memory
-    //Possible optimization: have multiple threads average the values in shared memory
-    //Need to __synchthreads() after the shared memory write
-    int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
-    __shared__ double subReductions[reducedArraySize];
-    
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    //How far into the array should the thread start?
-    int startingIndex = reductionFactor * tid;
-    double balsamic = 0;
-    for (int ndx = 0; ndx < reductionFactor; ndx++) {
-        balsamic += *(fullArray + ((startingIndex + ndx) % fullArraySize));
+    //Then make a single pass over the resulting array, wrapping around to pad the data as necessary
+    if (tid < reducedArraySize) {
+        odata[tid] = (sdata[tid] + sdata[(tid + reducedArraySize) % i]) / 2;
     }
-    balsamic /= reductionFactor;
-    *(reducedArray + tid) = balsamic;
 }
 
+//Fills an array with a repeating sequence of the integers from 1 to 100
+//Primarily useful for testing. Does not generate randomly distributed data
 void fillArray(double* target, int length) {
     for (int ndx = 0; ndx < length; ndx++) {
-        *(target + ndx) = ndx;
+        *(target + ndx) = ndx % 100;
     }
 }
 
+void fillArrayRandom(double* target, int length) {
+    std::mt19937 rng(time(0)); //Create a Mersenne Twister random number generator and seed it with the current time: https://www.guyrutenberg.com/2014/05/03/c-mt19937-example/
+    std::uniform_int_distribution<std::mt19937::result_type>rand1to100(1, 100);
+    for (int ndx = 0; ndx < length; ndx++) {
+        *(target + ndx) = rand1to100(rng);
+    }
+}
+
+
+
+//outputs an array in rows of 20 values
 void printArray(double* target, int length) {
     for (int ndx = 0; ndx < length; ndx++) {
        std::cout << std::setw(7) << std::fixed << std::setprecision(2) << *(target + ndx);
+       if (!((ndx+1) % 20)) {
+            std::cout << "\n";
+        }
     }
     std::cout << "\n";
 }
@@ -112,11 +101,24 @@ void printArray(double* target, int length) {
 int main() {
     double *fullArray, *reducedArray;
     double *dev_fullArray, *dev_reducedArray;
-    int reductionFactor = 2;
-    int fullArraySize = 6;
+    int fullArraySize = 1<<25;
+    int reductionFactor = 1<<24;
+    //int reductionFactor = fullArraySize;
     int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
-    int threadsPerBlock = reducedArraySize;
+    int threadsPerBlock = 1024;
     int blocks = 1;
+
+    //unsigned int n = 100;//# of integers
+    //unsigned int r = 100; //reduction factor
+    //int q = (n + (r - 1)) / r; //number of elements in the final array
+    int data_per_thread = (fullArraySize + threadsPerBlock - 1) / threadsPerBlock;
+    int smSize = threadsPerBlock*sizeof(double);//shared mem
+
+    //Timing variables
+    cudaEvent_t start, stop; 
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    float elapsedMs;
 
     //Allocate host memory
     fullArray = (double *)malloc(sizeof(double) * fullArraySize);
@@ -126,18 +128,40 @@ int main() {
     cudaMalloc((void**)&dev_fullArray, fullArraySize * sizeof(double));
     cudaMalloc((void**)&dev_reducedArray, reducedArraySize * sizeof(double));
 
-    fillArray(fullArray, fullArraySize);
-    printArray(fullArray, fullArraySize); 
+    fillArrayRandom(fullArray, fullArraySize);
+    //printArray(fullArray, fullArraySize); 
 
+    //Test the naive kernel
     //Copy data from host to device
     cudaMemcpy(dev_fullArray, fullArray, fullArraySize * sizeof(double), cudaMemcpyHostToDevice);
-
-    //Run kernel
+    //start timer
+    cudaEventRecord(start);
+    //Run naive kernel
     reduceArraySingleStep<<<blocks, threadsPerBlock>>>(dev_fullArray, dev_reducedArray, fullArraySize, reductionFactor); 
-
+    //stop timer 
+    cudaEventRecord(stop);
     //Copy data from device to host
     cudaMemcpy(reducedArray, dev_reducedArray, reducedArraySize * sizeof(double), cudaMemcpyDeviceToHost);
-   
+    //Calculate and display elapsed time
+    cudaEventElapsedTime(&elapsedMs, start, stop);
+    std::cout << "The naive kernel took " << elapsedMs << "ms to reduce " << fullArraySize << " doubles down to " << reducedArraySize << " doubles.\n";
+    printArray(reducedArray, reducedArraySize);
+
+    //Test the single block kernel 
+    //Copy data from host to device
+    cudaMemcpy(dev_fullArray, fullArray, fullArraySize * sizeof(double), cudaMemcpyHostToDevice);
+    //start timer
+    cudaEventRecord(start);
+    //Run single block kernel
+    reduceBlock<<<blocks, threadsPerBlock, smSize>>>(dev_fullArray, dev_reducedArray, fullArraySize, data_per_thread, reducedArraySize); 
+    //stop timer 
+    cudaEventRecord(stop);
+    //Copy data from device to host
+    cudaMemcpy(reducedArray, dev_reducedArray, reducedArraySize * sizeof(double), cudaMemcpyDeviceToHost);
+    //Calculate and display elapsed time
+    cudaEventElapsedTime(&elapsedMs, start, stop);
+    std::cout << "The single block kernel took " << elapsedMs << "ms to reduce " << fullArraySize << " doubles down to " << reducedArraySize << " doubles.\n";
+ 
     printArray(reducedArray, reducedArraySize);
 
     cudaFree(dev_fullArray);
