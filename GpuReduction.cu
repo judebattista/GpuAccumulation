4d005a895557312b4a45749442f09e6afd4f053c
judebattista <jbattista20@my.whitworth.edu>
2018-12-01 17:20:31 -0800

    Start optimizing approaches

--- GpuReduction.cu
+++ GpuReduction.cu
@@ -4,10 +4,88 @@
 
 //CUDA Kernel
 //Maps one thread to each output space
-//Should not be optimal for large reductionFactors
-__global__ void reduceArrayOutputMap(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactor) {
-    //int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
+//Reduces the array once by a factor of reductionFactor
+//Assumption: we have enough threads to span the output array for our given reductionFactor
+__global__ void reduceArraySingleStep(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactor) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    //If we are in our problem space
+    int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
+    if (tid < reducedArraySize) {
+        //How far into the array should the thread start?
+        int startingIndex = reductionFactor * tid;
+        double balsamic = 0;
+        for (int ndx = 0; ndx < reductionFactor; ndx++) {
+            balsamic += *(fullArray + ((startingIndex + ndx) % fullArraySize));
+        }
+        balsamic /= reductionFactor;
+        *(reducedArray + tid) = balsamic;
+    }
+}
+
+//Goals: 
+//No more than one shared memory load per item
+//Optimize thread use
+//Optimize memory coherence
+
+//CUDA Kernel
+//Maps one thread to each output space
+//Can reduce the array to a single value
+//Note that this reductionFactor applies to the complete reduction, not a single step.
+//Each thread works over a single section  of the array, so the memory access is poorly optimized
+//Each thread reduces it's section of the array to a single value via multiple iterations, which is then stored in the output array/
+//Much like the single step variant, but solves the problem of insufficient threads to span the array
+__global__ void reduceArrayOutputMap(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactori) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    //if we are in our problem space
+    if (tid < fullArraySize) {
+        //Figure out how many threads are working on the array
+        int numThreads = gridDim.x * blockDim.x;
+        //If numThreads >= reductionFactor, this is just the single step version
+        //Note that this check will be identical throughout a block, preventing divergence
+        if (numThreads >= reductionFactor) {
+            int startingIndex = reductionFactor * tid;
+            double balsamic = 0;
+            for (int ndx = 0; ndx < reductionFactor; ndx++) {
+                balsamic += *(fullArray + ((startingIndex + ndx) % fullArraySize));
+            }
+            balsamic /= reductionFactor;
+            *(reducedArray + tid) = balsamic;
+        } else { //Otherwise each thread will need to perform multiple reductions
+            //Based on the number of threads, figure out the size of the section each thread is responsible for
+            int sectionLen = (fullArraySize + numThreads - 1) / numThreads;
+            //Figure out where this thread should start its section
+            int sectionStart = sectionLen * tid; 
+            int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
+            double gravy = 0;
+            for (int ndx = 0; ndx < reductionFactor; ndx++) {
+                gravy += *(fullArray + ((startingIndex + ndx) % fullArraySize));
+            }
+            gravy /= reductionFactor;
+            *(reducedArray + tid) = gravy;
+        }
+    }
+}
+
+
+//CUDA Kernel
+//Can reduce the array to a single value
+//Note that this reductionFactor applies to the complete reduction, not a single step.
+//Each SET of threads spans a single segment, meaning that memory access for a given segment should be contiguous
+//This method does not require having enough threads to span the entire array
+__global__ void reduceArrayOutputMap(double* fullArray, double* reducedArray, int fullArraySize, int reductionFactori) {
+    int numThreads = gridDim.x * blockDim.x;
+    //We want each block of threads to address a specific section of the array of size N / numberOfBlocks
+    //Within that section, we want the set of threads in the block to span a segment of size section / numberOfThreadsPerBlock
+    //  and then move to another segment together until the section is complete
+    //Once each thread as averaged its values, write those values to shared memory
+    //Then thread 0 averages the values in shared memory
+    //Possible optimization: have multiple threads average the values in shared memory
+    //Need to __synchthreads() after the shared memory write
+    int reducedArraySize = (fullArraySize + reductionFactor - 1) / reductionFactor;
+    __shared__ double subReductions[reducedArraySize];
+    
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    
     //How far into the array should the thread start?
     int startingIndex = reductionFactor * tid;
     double balsamic = 0;
@@ -55,7 +133,7 @@ int main() {
     cudaMemcpy(dev_fullArray, fullArray, fullArraySize * sizeof(double), cudaMemcpyHostToDevice);
 
     //Run kernel
-    reduceArrayOutputMap<<<blocks, threadsPerBlock>>>(dev_fullArray, dev_reducedArray, fullArraySize, reductionFactor); 
+    reduceArraySingleStep<<<blocks, threadsPerBlock>>>(dev_fullArray, dev_reducedArray, fullArraySize, reductionFactor); 
 
     //Copy data from device to host
     cudaMemcpy(reducedArray, dev_reducedArray, reducedArraySize * sizeof(double), cudaMemcpyDeviceToHost);
